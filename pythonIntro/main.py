a9e69b3195ef16fe2da2429bbc96fe1f353a94cf
fontebo <fonteboachah@gmail.com>
2024-03-28 12:50:44 +0100

    first commit files from GDSC program

---
+++ pythonIntro/main.py
@@ -0,0 +1,17 @@
+house = "FonteboAchah"
+
+print(f"Hello")
+
+
+def test():
+    return "Hello world"
+
+
+items = ["egg", "spaghetti", "oil", "maggi and salt", "spices"]
+
+new = items[:4]
+
+new_list = items + new
+
+print(new_list)
+
